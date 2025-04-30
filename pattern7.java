dbf2aa4eb6495de749e928288d809100acbc53d4
atanu mandal <atanu2697@gmail.com>
2025-04-30 14:15:54 +0530

    corrected

--- pattern7.java
+++ pattern7.java
@@ -9,9 +9,9 @@ public class pattern7 {
         int n = 5;
 
         // outer loop
-        for (int  i = n; i >= 1; i--) {
+        for (int  i = 1; i <= n; i++) {
             //inner loop
-            for(int j = 1; j <= i;j++) {
+            for(int j = 1; j <= n-i+1;j++) {
                 System.out.print(j);
             }
             System.out.println();
