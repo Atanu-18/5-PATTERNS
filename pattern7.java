42582e9f161b84e4c56a008fc6f5af774c06bca9
atanu mandal <atanu2697@gmail.com>
2025-04-30 14:11:41 +0530

    Inverted Half Pyramid with Numbers

---
+++ pattern7.java
@@ -0,0 +1,20 @@
+// Inverted Half Pyramid with Numbers
+// 12345
+// 1234
+// 123
+// 12
+// 1
+public class pattern7 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // outer loop
+        for (int  i = n; i >= 1; i--) {
+            //inner loop
+            for(int j = 1; j <= i;j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
